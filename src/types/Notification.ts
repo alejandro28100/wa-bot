4199cd49944dc4389c17d1a2ecbc0d6fc97737b8
Alejandro Fuentes <62494669+alejandro28100@users.noreply.github.com>
2022-08-16 02:11:25 -0500

    Add notification type

---
+++ src/types/Notification.ts
@@ -0,0 +1,49 @@
+import { MessageTypes } from "./index";
+
+export type Notification = {
+  object: "whatsapp_business_account";
+  entry: NotificationEntry[];
+};
+
+type NotificationEntry = {
+  /** Your whatsapp business account id */
+  id: string;
+  changes: MessagesNotificationChange[];
+};
+
+type MessagesNotificationChange = {
+  value: NotificationChangeValue[];
+  field: "messages";
+};
+
+type NotificationChangeValue = {
+  metadata: ChangeMetadata;
+} & (MessagesNotificationChangeValue | StatusesNotificationChangeValue);
+
+type ChangeMetadata = {
+  display_phone_number: string;
+  phone_number_id: string;
+};
+
+type StatusesNotificationChangeValue = {
+  statuses: Status[];
+  messages: never;
+};
+
+type Status = {
+  status: string;
+  timestamp: number;
+  id: string;
+  recipient_id: string;
+};
+
+type MessagesNotificationChangeValue = {
+  messages: Message[];
+  statuses: never;
+};
+
+type Message = MessageTypes & {
+  from: string;
+  id: string;
+  timestamp: number;
+};
