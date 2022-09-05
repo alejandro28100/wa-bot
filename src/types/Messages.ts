f64b43b6e6a24229b8aa442a57cc83d8009f6fb0
Alejandro Fuentes <62494669+alejandro28100@users.noreply.github.com>
2022-09-04 19:01:30 -0500

    Add InteractiveMessageBase type to represent the multiple intactive messages

--- src/types/Messages.ts
+++ src/types/Messages.ts
@@ -23,30 +23,41 @@ type MediaMessage = Message & {
   };
 };
 
-type InteractiveMessage = Message & {
+/**An interactive message can represent either:
+ *
+ *  When a user clicks a reply button sent
+ * {@link AnswerFromListMessage}
+ *
+ * When a user clicks on an item from a list message
+ * {@link ReplyButtonMessage}
+ * */
+export type InteractiveMessage = AnswerFromListMessage | ReplyButtonMessage;
+
+export type InteractiveMessageBase<T> = Message & {
   type: "interactive";
-  interactive: AnswerFromListMessage | ReplyButtonMessage;
+  interactive: T;
 };
 
-/** Response of a reply button sent */
-type ReplyButtonMessage = {
+/** When a user clicks a reply button you sent */
+export type ReplyButtonMessage = InteractiveMessageBase<{
   button_reply: {
     id: string;
     title: string;
   };
   type: "button_reply";
-};
+}>;
 
-type AnswerFromListMessage = {
+/**When a user clicks on an item from a list message */
+export type AnswerFromListMessage = InteractiveMessageBase<{
   list_reply: {
     id: string;
     title: string;
     description: string;
   };
   type: "list_reply";
-};
+}>;
 
-/** Quick reply button in an interactive message template */
+/** When a user clicks a reply button in an interactive message template */
 export type ButtonMessage = {
   type: "button";
   button: {
